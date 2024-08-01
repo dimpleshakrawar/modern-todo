59c39c93dd0abfa7f50e9a03f0a157e6bdaabdd0
dimpleshakrawar <dimpleshakrawar@gmail.com>
2024-08-02 00:54:32 +0530

    image fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,12 @@
 const Login = () => {
      return (
           <div
-               className="bg-[url('/assets/loginPage.jpg')]"
-               //    style={{
-               //         backgroundImage: 'url(../assets/loginPage.jpg)',
-               //         height: '100vh',
-               //         backgroundSize: 'cover',
-               //         width: '100vw'
-               //    }}
+               style={{
+                    backgroundImage: 'url(/assets/loginPage.jpg)',
+                    height: '100vh',
+                    backgroundSize: 'cover',
+                    width: '100vw'
+               }}
           >
                Login
           </div>
